8ebf6d091002656fc657e6d41aa65ffa05060af3
wittenber0 <ryanwwittenberg@gmail.com>
2017-06-27 13:08:45 -0400

    ViewableData

---
+++ src/NewViewScreenController.java
@@ -0,0 +1,33 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ * Created by FMF 7 on 6/27/2017.
+ */
+public class NewViewScreenController {
+    public ListView allTablesView;
+    public ListView viewTablesView;
+    public ListView allColumnsView;
+    public ListView uniqueKeyView;
+    public Button setKeyButton;
+    public Button saveViewButton;
+
+    public void onBack(ActionEvent actionEvent) {
+    }
+
+    public void onLogout(ActionEvent actionEvent) {
+    }
+
+    public void onAddTable(ActionEvent actionEvent) {
+    }
+
+    public void onRemoveTable(ActionEvent actionEvent) {
+    }
+
+    public void onSetKey(ActionEvent actionEvent) {
+    }
+
+    public void onSaveView(ActionEvent actionEvent) {
+    }
+}
